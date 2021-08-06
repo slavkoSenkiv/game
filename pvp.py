c0b52ba149e589867ebd1d2fdc52df79bfba2fba
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-08-06 19:18:22 +0300

    Update pvp.py

--- pvp.py
+++ pvp.py
@@ -1,38 +1,46 @@
-# player = [0 health,  1 defence, 2 damage]
+# here list values represent player characteristics
+# player = [0 health,  1 defence, 2 damage, 3 name]
 player_A = [1000, 50, 200, 'player_A']
 player_B = [1000, 50, 300, 'player_B']
 
 
-def damage(attacker, victim):
-    # damage = attack * (100/(100 + defence))
-    # hpLost = hpLost - damage
-    victim[0] = victim[0] - (attacker[2] * (100/(100 + victim[1])))
+def hp_left(attacker, victim):
+    # this function calculate the attacked player HP los
+    # attacked_player_HP = attacked_player_HP - attacking_player_damage
+    # attacking_player_damage = attacking_player_attack * (100/(100 + attacked_player_defence))
+
+    damage = attacker[2] * (100/(100 + victim[1]))
+    print(f'the damage from {attacker[3]} to {victim[3]} = {int(damage)}')
+
+    victim[0] = int(victim[0] - damage)
+    print(f'{victim[3]} HP = {victim[0]}\n')
+
     return victim[0]
 
-def whoWon(playerA, playerB):
-    if playerA[0] <= 0:
-        won = playerA
-        print(f'won {won[3]}')
 
-    if playerB[0] <= 0:
-        won = playerB
+def who_won(player_a, player_b):
+    # this function checks if someone win and who exactly
+    # if player1's HP <= 0 then player2 is winner
+    if player_a[0] <= 0:
+        won = player_b
         print(f'won {won[3]}')
+        exit()
 
+    if player_b[0] <= 0:
+        won = player_a
+        print(f'won {won[3]}')
+        exit()
 
 
 print('player_A = ', player_A)
 print('player_B = ', player_B, '\n')
 
-
 while player_B[0] >= 0 or player_A[0] >= 0:
-    print('here goes damage from A to B')
-    damage(player_A, player_B)
-    whoWon(player_A, player_B)
-    print('player_B = ', player_B)
 
-    print('here goes damage from B to A')
-    damage(player_B, player_A)
-    whoWon(player_A, player_B)
-    print('player_A = ', player_A, '\n')
+    hp_left(player_A, player_B)
+    who_won(player_A, player_B)
+
+    hp_left(player_B, player_A)
+    who_won(player_A, player_B)
 
 
